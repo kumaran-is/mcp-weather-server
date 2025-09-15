aff929c55d2f413a2fc86dadf358ae0b9e7c83ab
kumaran Iyyasamy srinivasan <kumaran.isk@gmail.com>
2025-09-15 00:34:03 -0400

    updated

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,9 +3,12 @@
  */
 
 import { readFileSync } from 'fs';
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// Get version from package.json using CommonJS approach
+// Get version from package.json using ES module approach
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const packageJsonPath = join(__dirname, '..', '..', 'package.json');
 const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
 
